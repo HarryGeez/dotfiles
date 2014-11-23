575b396ea12355e0f1a986466eca57230b3c1f8f
Wei Jian Gan <weijiangan@outlook.com>
2014-11-24 02:34:43 +0800

    Modified emacs' init.el to match my MacBook's config.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,7 +1,10 @@
-(require 'package)
-(add-to-list 'package-archives
-	     '("melpa" . "http://melpa.org/packages/") t)
-(package-initialize)
+; (add-to-list 'load-path "~/.emacs.d/lisp")
+(when (> emacs-major-version 23)
+  (require 'package)
+  (add-to-list 'package-archives
+    '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  (package-initialize))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -14,13 +17,29 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; Autopair
 (require 'autopair)
 (autopair-global-mode)
-;; (add-hook 'c-mode-common-hook
-;; 	  #'(lambda () (autopair-mode)))
+
+;; C Programming
+; Set C-Mode to use Stroustrup indentation style.
+(setq c-default-style "stroustrup")
+; More indentation settings; Set tab-width to 4.
 (setq-default c-basic-offset 4
-	      tab-width 4
-	      indent-tabs-mode t)
+			  tab-width 4
+			  indent-tabs-mode t)
+; Map "newline-and-indent" to RET
 (require 'cc-mode)
 (define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
-(setq c-default-style "stroustrup")
+
+;; Smart-Tabs-Mode
+(autoload 'smart-tabs-mode "smart-tabs-mode"
+  "Intelligently indent with tabs, align with spaces!")
+(autoload 'smart-tabs-mode-enable "smart-tabs-mode")
+(autoload 'smart-tabs-advice "smart-tabs-mode")
+(autoload 'smart-tabs-insinuate "smart-tabs-mode")
+; Smart-Tabs for C
+(add-hook 'c-mode-hook 'smart-tabs-mode-enable)
+(smart-tabs-advice c-indent-line c-basic-offset)
+(smart-tabs-advice c-indent-region c-basic-offset)
