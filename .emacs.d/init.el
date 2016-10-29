33c1c2d8bc77dc41ae87719532519493666573a0
Wei Jian Gan <weijiangan@outlook.com>
2016-10-29 11:52:22 +0800

    Revamp of Emacs experience!

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,19 +1,20 @@
 ;; (add-to-list 'load-path "~/.emacs.d/lisp")
+(setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
 (when (> emacs-major-version 23)
   (require 'package)
   (add-to-list 'package-archives
 			   '("melpa" . "http://melpa.milkbox.net/packages/") t)
   (package-initialize)
-  
+
   (defun ensure-package-installed (&rest packages)
-	"Assure every package is installed, ask for installation if it’s not. 
-Return a list of installed packages or nil for every skipped package."
+	"Assure every package is installed, ask for installation if it’s not.
+    Return a list of installed packages or nil for every skipped package."
 	(mapcar
 	 (lambda (package)
 	   ;; (package-installed-p 'evil)
 	   (if (package-installed-p package)
 		   nil
-		 (if (y-or-n-p (format "Package %s is missing. Install it? " package))
+		 (if (y-or-n-p (format "Package %s is missing.  Install it? " package))
 			 (package-install package)
 		   package)))
 	 packages))
@@ -24,14 +25,49 @@ Return a list of installed packages or nil for every skipped package."
 	  (package-refresh-contents))
 
   (ensure-package-installed
-   'iedit
-   'web-mode
+   'base16-theme
+   'company
+   'company-c-headers
+   'company-irony
+   'company-tern
+   'delight
    'emmet-mode
    'expand-region
+   'golden-ratio
+   'helm
+   'iedit
+   'irony
+   'js2-mode
+   'persp-mode
+   'spaceline
+   'web-mode
+   'which-key
+   'window-numbering
    'yasnippet
-   'workgroups2
-   'auto-complete
    ))
+
+(if (display-graphic-p)
+    (progn
+	  (load-theme 'base16-ocean t)
+	  ;; (defun invisible-fringes ()
+	  ;; 	(set-face-attribute 'fringe nil
+	  ;; 						:foreground (face-foreground 'default)
+	  ;; 						:background (face-background 'default)))
+	  ;; (invisible-fringes)
+	  (set-frame-font "Menlo 13")
+	  ;; This is bound to f11 in Emacs 24.4
+	  (toggle-frame-fullscreen)
+	  ;; Who use the bar to scroll?
+	  (scroll-bar-mode 0)
+	  ;; No toolbar
+	  (tool-bar-mode 0)
+	  ;; ;; No menubar
+	  ;; (menu-bar-mode 0)
+	  ;; No fringes
+	  (fringe-mode 0))
+  ;; else
+  ;; No menubar
+  (menu-bar-mode 0))
 
 ;; Disable startup message
 (setq inhibit-startup-message t)
@@ -41,22 +77,19 @@ Return a list of installed packages or nil for every skipped package."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(initial-scratch-message nil))
+ '(company-c-headers-path-system
+   (quote
+	("/usr/include/" "/usr/local/include/" "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/../include/c++/v1/")))
+ '(initial-scratch-message nil)
+ '(package-selected-packages
+   (quote
+	(delight general-close which-key company-irony irony helm persp-mode spaceline window-numbering company-tern company-c-headers company js2-mode base16-theme yasnippet web-mode typing iedit expand-region emmet-mode 2048-game))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-;; ;; This is bound to f11 in Emacs 24.4
-;; (toggle-frame-fullscreen)
-;; ;; Who use the bar to scroll?
-;; (scroll-bar-mode 0)
-;; ;; No toolbar
-;; (tool-bar-mode 0)
-;; No menubar
-(menu-bar-mode 0)
 
 ;; Compile witout prompt. Add C-u before M-x compile to compile normally
 (setq compilation-read-command nil)
@@ -68,7 +101,7 @@ Return a list of installed packages or nil for every skipped package."
 
 ;; Clear Eshell buffer
 (defun eshell-clear-buffer ()
-  "Clears the shell buffer ala Unix's clear or DOS' cls"
+  "Clears the shell buffer ala Unix's clear or DOS' cls."
   (interactive)
   ;; the shell prompts are read-only, so clear that for the duration
   (let ((inhibit-read-only t))
@@ -80,24 +113,31 @@ Return a list of installed packages or nil for every skipped package."
 		  '(lambda()
 			 (local-set-key (kbd "C-l") 'eshell-clear-buffer)))
 
+(defun fake-kill-line ()
+  "Save the rest of the current line to the 'kill-ring'."
+  (interactive)
+  (save-excursion
+	(set-mark-command nil)
+	(move-end-of-line 1)
+	(kill-ring-save (region-beginning) (region-end))))
+
+(global-set-key (kbd "C-c k") 'fake-kill-line)
+
 
 ;;
-;;  Electric-Pair Mode
+;;  smartparens-mode
 ;;
 
-(electric-pair-mode 1)
+(require 'smartparens-config)
+(smartparens-strict-mode)
+(smartparens-global-mode 1)
 
-;; ;; Only enable electric-pair for specified modes
-;; (defvar my-electic-pair-modes '(c-mode))
-;; (defun my-inhibit-electric-pair-mode (char)
-;;   (not (member major-mode my-electic-pair-modes)))
-;; (setq electric-pair-inhibit-predicate #'my-inhibit-electric-pair-mode)
 
 ;;
 ;;  C-Mode
 ;;
 
-;; Set C-Mode to use Stroustrup indentation style.
+;; Set C-Mode to use Linux indentation style.
 (setq c-default-style "linux")
 ;; More indentation settings; Set tab-width to 4.
 (setq-default c-basic-offset 4
@@ -107,19 +147,19 @@ Return a list of installed packages or nil for every skipped package."
 (require 'cc-mode)
 (define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
 
-;; Function to run command in an eshell buffer if compilation finishes without errors.
-(defun run-compilation-output-in-eshell (buf msg)
-  "If compilation finished successfully, switch to eshell and execute a command."
-  (interactive)
-  (when (equal major-mode 'compilation-mode)
-	(when (string= msg "finished\n")
-	  (other-window 1)
-	  (eshell)
-	  (goto-char (point-max))
-	  (eshell-kill-input)
-	  (insert "./a.out")
-	  (eshell-send-input)
-	  (other-window 1))))
+;; ;; Function to run command in an eshell buffer if compilation finishes without errors.
+;; (defun run-compilation-output-in-eshell (buf msg)
+;;   "If compilation finished successfully, switch to eshell and execute a command."
+;;   (interactive)
+;;   (when (equal major-mode 'compilation-mode)
+;; 	(when (string= msg "finished\n")
+;; 	  (other-window 1)
+;; 	  (eshell)
+;; 	  (goto-char (point-max))
+;; 	  (eshell-kill-input)
+;; 	  (insert "./a.out")
+;; 	  (eshell-send-input)
+;; 	  (other-window 1))))
 
 ;; Change default compile command to cc <buffer name>.
 (add-hook 'c-mode-hook
@@ -128,8 +168,32 @@ Return a list of installed packages or nil for every skipped package."
 			  (set (make-local-variable 'compile-command)
 				   (concat "gcc " (shell-quote-argument buffer-file-name))))))
 
-;; Run a.out in an eshell buffer after compilation.
-(add-hook 'compilation-finish-functions 'run-compilation-output-in-eshell)
+;; ;; Run a.out in an eshell buffer after compilation.
+;; (add-hook 'compilation-finish-functions 'run-compilation-output-in-eshell)
+
+
+;;
+;;  c++-mode
+;;
+
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+(add-hook 'c++-mode-hook
+		  (lambda ()
+			(when (not (equal buffer-file-name nil))
+			  (set (make-local-variable 'compile-command) "g++ *.cc"))))
+
+
+;; ;;
+;; ;;  semantic
+;; ;;
+
+;; (require 'semantic)
+;; (global-semanticdb-minor-mode 1)
+;; (global-semantic-idle-scheduler-mode 1)
+;; (semantic-mode 1)
+;; (semantic-add-system-include "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/../include/c++/v1" 'c++-mode)
+;; (semantic-add-system-include "~/linux/kernel")
+;; (semantic-add-system-include "~/linux/include")
 
 
 ;;
@@ -144,7 +208,7 @@ Return a list of installed packages or nil for every skipped package."
 
 
 ;;
-;;  expand-region  
+;;  expand-region
 ;;
 
 (require 'expand-region)
@@ -166,8 +230,7 @@ Return a list of installed packages or nil for every skipped package."
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
 
 (add-hook 'web-mode-hook
 		  '(lambda()
@@ -176,17 +239,6 @@ Return a list of installed packages or nil for every skipped package."
 			 (setq web-mode-script-padding 2)
 			 (setq web-mode-code-indent-offset 2)
 			 (setq web-mode-enable-css-colorization t)))
-
-(setq web-mode-content-types-alist
-	  '(("jsx" . "\\.js[x]?\\'")))
-
-(defun web-mode-jsx-fix ()
-  "Fix web-mode's broken JSX syntax highlighting and reindent buffer."
-  (interactive)
-  (when (equal major-mode 'web-mode)
-	(web-mode)
-	(indent-region (point-min) (point-max))
-	(delete-trailing-whitespace)))
 
 (define-key web-mode-map (kbd "C-c C-i") nil)
 (define-key web-mode-map (kbd "C-c C-i") 'web-mode-jsx-fix)
@@ -210,16 +262,7 @@ Return a list of installed packages or nil for every skipped package."
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
-
-
-;;
-;;  workgroups2
-;;
-
-(require 'workgroups2)
-(setq wg-prefix-key (kbd "C-c z"))
-;; (workgroups-mode 1)
-;; (wg-open-session "~/.emacs_workgroup")
+(setq org-agenda-files '("~/.org"))
 
 
 ;;
@@ -230,11 +273,198 @@ Return a list of installed packages or nil for every skipped package."
 
 
 ;;
-;;  auto-complete
+;;  company
 ;;
 
-;; start auto-complete with emacs
-(require 'auto-complete)
-;; do default config for auto-complete
-(require 'auto-complete-config)
-(ac-config-default)
+(require 'company)
+(add-hook 'after-init-hook 'global-company-mode)
+(add-to-list 'company-backends 'company-irony)
+(add-to-list 'company-backends 'company-c-headers)
+(add-to-list 'company-backends 'company-tern)
+
+;; ;; begin company and yasnippet conflict fix
+;; (defun check-expansion ()
+;;   (save-excursion
+;; 	(if (looking-at "\\_>") t
+;; 	  (backward-char 1)
+;; 	  (if (looking-at "\\.") t
+;; 		(backward-char 1)
+;; 		(if (looking-at "->") t nil)))))
+
+;; (defun do-yas-expand ()
+;;   (let ((yas/fallback-behavior 'return-nil))
+;; 	(yas/expand)))
+
+;; (defun tab-indent-or-complete ()
+;;   (interactive)
+;;   (if (minibufferp)
+;; 	  (minibuffer-complete)
+;; 	(if (or (not yas/minor-mode)
+;; 			(null (do-yas-expand)))
+;; 		(if (check-expansion)
+;; 			(company-complete-common)
+;; 		  (indent-for-tab-command)))))
+
+;; (global-set-key [tab] 'tab-indent-or-complete)
+;; ;; end fix
+
+
+;;
+;;  irony
+;;
+
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
+
+;; replace the `completion-at-point' and `complete-symbol' bindings in
+;; irony-mode's buffers by irony-mode's function
+(defun my-irony-mode-hook ()
+  (define-key irony-mode-map [remap completion-at-point]
+    'irony-completion-at-point-async)
+  (define-key irony-mode-map [remap complete-symbol]
+    'irony-completion-at-point-async))
+(add-hook 'irony-mode-hook 'my-irony-mode-hook)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
+(setq irony-additional-clang-options
+	  (quote
+	   ("-I/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/../include/c++/v1"
+		"-I/usr/local/include"
+		"-I/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/../lib/clang/8.0.0/include"
+		"-I/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include"
+		"-I/usr/include")))
+
+
+;;
+;;  js2-mode
+;;
+
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+
+
+;;
+;;  tern-mode
+;;
+
+(add-to-list 'load-path "/usr/local/lib/node_modules/tern/emacs/")
+(autoload 'tern-mode "tern.el" nil t)
+(add-hook 'js2-mode-hook (lambda () (tern-mode t)))
+
+
+;;
+;;  persp-mode
+;;
+
+(with-eval-after-load "persp-mode-autoloads"
+  (setq wg-morph-on nil) ;; switch off animation
+  (setq persp-autokill-buffer-on-remove 'kill-weak)
+  (add-hook 'after-init-hook #'(lambda () (persp-mode 1))))
+
+
+;;
+;;  window-numbering-mode
+;;
+
+(defun window-numbering-install-mode-line (&optional position)
+  "Do nothing.")
+(window-numbering-mode)
+
+
+;;
+;;  spaceline
+;;
+
+(setq powerline-default-separator 'utf-8) ; fix off-colors, must be in this order!
+(require 'spaceline-config)
+(spaceline-spacemacs-theme)
+
+
+;;
+;;  helm
+;;
+
+(require 'helm-config)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-c h o") 'helm-occur)
+(setq helm-buffers-fuzzy-matching t
+      helm-recentf-fuzzy-match    t)
+(helm-mode 1)
+(when (executable-find "curl")
+  (setq helm-google-suggest-use-curl-p t))
+
+(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t
+      helm-echo-input-in-header-line t)
+
+(defun spacemacs//helm-hide-minibuffer-maybe ()
+  "Hide minibuffer in Helm session if we use the header line as input field."
+  (when (with-helm-buffer helm-echo-input-in-header-line)
+    (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
+      (overlay-put ov 'window (selected-window))
+      (overlay-put ov 'face
+                   (let ((bg-color (face-background 'default nil)))
+                     `(:background ,bg-color :foreground ,bg-color)))
+      (setq-local cursor-type nil))))
+
+
+(add-hook 'helm-minibuffer-set-up-hook
+          'spacemacs//helm-hide-minibuffer-maybe)
+
+(setq helm-autoresize-max-height 0)
+(setq helm-autoresize-min-height 20)
+(helm-autoresize-mode 1)
+
+;;
+;;  which-key
+;;
+
+(require 'which-key)
+(which-key-mode)
+(which-key-setup-side-window-bottom)
+
+
+;;
+;;  golden-ratio
+;;
+
+(require 'golden-ratio)
+(setq golden-ratio-extra-commands
+      (append golden-ratio-extra-commands
+              '(select-window-0
+				select-window-1
+				select-window-2
+				select-window-3
+				select-window-4
+				select-window-5
+				select-window-6
+				select-window-7
+				select-window-8
+				select-window-9)))
+(golden-ratio-mode 1)
+
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+
+;;
+;;  delight
+;;
+
+(require 'delight)
+(delight '((company-mode "Ⓐ" company)
+           (smartparens-mode "ⓟ" smartparens)
+		   (yas-minor-mode "ⓨ" yasnippet)
+		   (golden-ratio-mode "ⓖ" golden-ratio)
+           (visual-line-mode "Ⓦ" simple)
+           (abbrev-mode "ⓐ" abbrev)
+           (helm-mode "Ⓗ" helm)
+		   (irony-mode "Ⓘ" irony)
+		   (which-key-mode nil which-key)
+		   (flycheck-mode nil flycheck)
+           ))
