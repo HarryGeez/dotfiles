3abd3f6b74c10bc326ce98a23300aab9cfe2f46e
Wei Jian Gan <weijiangan@outlook.com>
2017-02-24 18:51:38 +0800

    added Cperl mode configs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -428,7 +428,22 @@
 				select-window-9)))
 ; (golden-ratio-mode 1)
 
+
+;;
+;;  flycheck
+;;
+
 (add-hook 'after-init-hook #'global-flycheck-mode)
+
+
+;;
+;;  Cperl-mode
+;;
+
+(add-to-list 'auto-mode-alist '("\\.\\([pP][Llm]\\|al\\)\\'" . cperl-mode))
+(add-to-list 'interpreter-mode-alist '("perl" . cperl-mode))
+(add-to-list 'interpreter-mode-alist '("perl5" . cperl-mode))
+(add-to-list 'interpreter-mode-alist '("miniperl" . cperl-mode))
 
 
 (if (display-graphic-p)
