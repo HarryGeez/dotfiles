c73da1c90cd58a48e87b9a25c875df594f084d5d
Wei Jian Gan <weijiangan@outlook.com>
2017-08-12 00:58:20 +0800

    [master] Emacs: removed useless lines in web-mode config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -187,8 +187,6 @@
 ;;  web-mode
 ;;
 
-;; The customisations below should not be put in the hook. Declare them before loading web-mode.el
-
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
@@ -207,8 +205,6 @@
 			 (setq web-mode-code-indent-offset 2)
 			 (setq web-mode-enable-css-colorization t)))
 
-(define-key web-mode-map (kbd "C-c C-i") nil)
-(define-key web-mode-map (kbd "C-c C-i") 'web-mode-jsx-fix)
 
 
 ;;
