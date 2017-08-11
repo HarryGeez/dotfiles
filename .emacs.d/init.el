fa6b4f23c7c9a45adf14923c116999bd4ab5b887
Wei Jian Gan <weijiangan@outlook.com>
2017-08-12 00:59:17 +0800

    [master] Emacs: web-mode indent CSS properly in <style>

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -200,9 +200,10 @@
 (add-hook 'web-mode-hook
 		  '(lambda()
 			 (setq web-mode-markup-indent-offset 2)
+			 (setq web-mode-css-indent-offset 2)
+			 (setq web-mode-code-indent-offset 2)
 			 (setq web-mode-style-padding 2)
 			 (setq web-mode-script-padding 2)
-			 (setq web-mode-code-indent-offset 2)
 			 (setq web-mode-enable-css-colorization t)))
 
 
