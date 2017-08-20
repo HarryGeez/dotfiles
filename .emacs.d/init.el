ecdee5652395b805d5c27e1370bf5a90a690ab84
Wei Jian Gan <weijiangan@outlook.com>
2017-08-20 10:35:43 +0800

    [master] Emacs: now auto installs missing packages w/o prompt

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -16,20 +16,14 @@
   (package-initialize)
 
   (defun ensure-package-installed (&rest packages)
-	"Assure every package is installed, ask for installation if it’s not.
-    Return a list of installed packages or nil for every skipped package."
+	"Install missing packages without prompting"
 	(mapcar
 	 (lambda (package)
-	   ;; (package-installed-p 'evil)
-	   (if (package-installed-p package)
-		   nil
-		 (if (y-or-n-p (format "Package %s is missing.  Install it? " package))
-			 (package-install package)
-		   package)))
+       (unless (package-installed-p package)
+         (package-install package)))
 	 packages))
 
-  ;; make sure to have downloaded archive description.
-  ;; Or use package-archive-contents as suggested by Nicolas Dudebout
+  ;; refresh package contents on first run ie elpa folder is missing
   (or (file-exists-p package-user-dir)
 	  (package-refresh-contents))
 
